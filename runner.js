3699ac1886b0b23cbac194cb1b067f5384b32ebb
vika2204 <viktoriya.kireeva22@gmail.com>
2024-10-18 16:04:00 +0300

    release play

--- runner.js
+++ runner.js
@@ -1,2 +1,14 @@
 // Используйте для решения судоку необходимые функции из файла sudoku.js
 const { read, parseSudoku, solveSudoku, isSolved, prettyBoard } = require('./sudoku');
+
+function solve() {
+    const sudokuText = read();
+    const sudokuArray = parseSudoku(sudokuText);
+    const solvedSudoku = solveSudoku(sudokuArray);
+    return solvedSudoku;
+  }
+  
+  // Пример использования
+  const solvedSudoku = solve(read);
+  console.log("Решено ли судоку:", isSolved(solvedSudoku));
+  prettyBoard(solvedSudoku);
